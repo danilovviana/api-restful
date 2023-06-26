9d729ed828e81521f9c8f5a6048d0c8078268eec
Danilo Viana <danilovviana@gmail.com>
2023-06-26 20:32:35 -0300

    adicionando metodo get, estudos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,15 +2,7 @@ const router = require('express').Router()
 
 const ProductController = require('../controllers/products')
 
-
-// VERBOS HTTP (4 TIPOS)
-// GET -  obter dados
-// POST - enviar/receber dados
-// PUT - atualizar dados
-// DELETE - remover dados
-
-
-router.get('/products/id:?', ProductController.get)
+router.get('/products/:id?', ProductController.get)
     // router.post('/products', ProductController.post)
     // router.put('/products/:id', ProductController.put)
     // router.delete('/products/:id', ProductController.delete)
